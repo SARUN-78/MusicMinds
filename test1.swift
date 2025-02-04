f959b203704d04b7ebb2c3a4cba3611740e22f55
SARUN-78 <arunkumar7792@gmail.com>
2025-02-04 21:23:45 +0100

    Create test1.swift

---
+++ test1.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tidetimes
+//
+//  Created by Arunkumar Angadinagaraja on 04.02.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
