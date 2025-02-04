bc6ae52c30dfd1101f6b9b5cb148dfd1725ab0ec
SARUN-78 <arunkumar7792@gmail.com>
2025-02-04 21:29:34 +0100

    Create tideTimes.swift

---
+++ tideTimes.swift
@@ -0,0 +1,17 @@
+//
+//  TidetimesApp.swift
+//  Tidetimes
+//
+//  Created by Arunkumar Angadinagaraja on 04.02.25.
+//
+
+import SwiftUI
+
+@main
+struct TidetimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
